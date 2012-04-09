4e0370e4264b6971f92ffc2d81fa70b9cbdf3d1e
Sam Birch <sam.m.birch@gmail.com>
2012-04-09 02:24:44 -0400

    templating javascript :o

--- whylearnit/site/exercise-content/videogames/videogames.js
+++ whylearnit/site/exercise-content/videogames/videogames.js
@@ -5,13 +5,42 @@ videogames.js
 @date 4.8.2012
 **************************************************/
 
-WhyLearnIt.packetLoaded({
-	id: 'videogames',
-	title: 'Videogames',
+WhyLearnIt.packetLoaded((function(){
+
+	var questions = [<% for question in questions %>'<%question%>'<% if not forloop.last%>, <% endif %><% endfor %>];
+	var hints = [<% for hint in hints %>'<%hint%>'<% if not forloop.last%>, <% endif %><% endfor %>];
+	var reponseTypes = [<% for responseType in responseTypes %>'<%responseType%>'<% if not forloop.last%>, <% endif %><% endfor %>];
+	var choices = [<% for choices in questionChoices %><% for choice in choices %>'<%choice%>'<% if not forloop.last%>, <% endif %><% endfor %><% if not forloop.last%>, <% endif %><% endfor %>];
+
+	var packet = {
+		id: 'videogames',
+		title: '<%title%>',
+		description: '<%description%>',
+		video: '<%video-link%>',
+		related: [<% for related_id in related %>'<%related_id%>'<% if not forloop.last%>, <% endif %><% endfor %>],
+		exercises: [] //see loop below for format
+	};
+
+	for(var i=0;i<questions.length;i++){
+		packet.exercises.push({
+			question: questions[i],
+			hint: hints[i],
+			responseType: responseTypes[i],
+			choices: choices[i]
+		});
+	}
+
+	return packet;
+})());
+
+
+
+{
+	
 	init: function(){
 
 	},
 	renderInteractiveContent: function(){
 		console.log('Hello, world!');
 	}
-});
+}
