248c07f94237bf0c126690eb3bca597817a1da66
Sam Birch <sam.m.birch@gmail.com>
2012-04-08 23:47:04 -0400

    initial of videogames

---
+++ whylearnit/site/exercise-content/videogames/videogames.js
@@ -0,0 +1,16 @@
+/******************************************************
+videogames.js
+
+@author sbirch
+@date 4.8.2012
+**************************************************/
+
+var JS_FILE = 'videogames.js';
+
+function init(){
+	
+}
+
+function renderInteractiveContent(){
+	console.log('Hello, world!');
+}
