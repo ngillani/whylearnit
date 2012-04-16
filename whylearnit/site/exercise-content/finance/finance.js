be94d9e493c7cbde9825bf36143113bdb61f2009
John Connuck <john_connuck@brown.edu>
2012-04-15 22:19:30 -0400

    finance1.js renamed to finance.js

--- whylearnit/site/exercise-content/finance/finance.js
+++ whylearnit/site/exercise-content/finance/finance.js
@@ -1,3 +1,4 @@
+
 /******************************************************
   finance.js
   Code for producing visualizations for finance exercises
@@ -6,13 +7,14 @@
   @author ngillani
   @date 3.30.12
 
-
+  @author jconnuck
+  @date 4.12.12
 
  **************************************************/
 
 {% extends "packet.js" %}
-{% block render_visual %}
-// Constants!
+{% block extra %}
+
 var CHART_WIDTH = 600;
 var CHART_HEIGHT = 300;
 
@@ -24,66 +26,110 @@ _allCompanyData['Infospace'] = '/site/exercise-content/finance/stock-data/infosp
 
 var _allPriceData = {};
 
+(function init () {
+  $.each(_allCompanyData, function(key, value) {
+    // Retrieve stock data and store closing prices
+    var request = $.ajax({type : "GET", url : value, async : false});
+    request.done(function(data){
+      var lines = data.split('\n');
 
-util = { 
-  init: function(){ // Crack open the csv files and aggregate data to plot
-          $.each(_allCompanyData, function(key, value){
-            // Retrieve stock data and store closing prices
-            var request = $.ajax({type : "GET", url : value, async : false});
-            request.done(function(data){
-              var lines = data.split('\n');
+      _allPriceData[key] = new Array();
 
-              _allPriceData[key] = new Array();
+      // Start at 1 to avoid the 'Close' text!
+      for(var i = 1; i < lines.length; i++){
+        _allPriceData[key].push(parseFloat(lines[i].split(',')[4]));
+      }
+    });
+  });	
+})();
 
-              // Start at 1 to avoid the 'Close' text!
-              for(var i = 1; i < lines.length; i++){
-                _allPriceData[key].push(parseFloat(lines[i].split(',')[4]));
-              }
-            });
-          });	
-        },
-  findMaxEntries: function(){
-                    var numPrices = 0;
-                    for(var company in _allPriceData){
-                      if (!_allPriceData.hasOwnProperty(company)){continue;}
-                      if(_allPriceData[company].length > numPrices){
-                        numPrices = _allPriceData[company].length;
-                      }
-                    }
+function drawChart(companiesToPlot, elementId){
+  var numPrices = findMaxEntries();
+  var data = new google.visualization.DataTable();
 
-                    _allPriceData['days'] = new Array();
-                    for(var i = 1; i <= numPrices; i++){
-                      _allPriceData['days'].push(i);
-                    }
+  // Add column for days
+  data.addColumn('string', 'days');
 
-                    return numPrices;
-                  },
-  renderInteractiveContent: function(elementId){
-                              // Build array of all company names
-                              var keyArray = [];
-                              for(key in _allCompanyData){
-                                keyArray.push(key);
-                              }
-                              this.drawChart(keyArray, elementId);
-                            },
-  retrieveSelectedCompany: function(){
-                             // First, get the company that the student has chosen
-                             var chosenCompany = "";
-                             $('.response-medium', '#exercise-1').each(function(){
-                               chosenCompany = $(this).find('input:radio:checked')[0].value;
-                             });
+  // Add columns for each company	
+  for(var company in companiesToPlot){
+    data.addColumn('number', companiesToPlot[company]);
+  }
 
-                             return chosenCompany;
-                           },
-  refreshAllExerciseContent: function(){ //Update the charts for problems 2 and 3 based on the selected company
-                               this.drawChart([this.retrieveSelectedCompany()], 'interactive-2');
-                               this.drawChart([this.retrieveSelectedCompany()], 'interactive-3');
-                             },
-  choiceOnClick: function(){ // Generic function, called whenever a radio button is selected
-                   this.refreshAllExerciseContent();
-                 }
-};
 
-util.init.call(util);
-var visuals = [ function () { util.renderInteractiveContent.call(util, 'interactive-1'); }]
-{% endblock render_visual %}
+  // Now, add rows
+  for(var i=0; i < numPrices; i++){
+
+    var rowArray = new Array();
+    rowArray.push(""+_allPriceData['days'][i]);
+
+    for(var company in companiesToPlot){
+      rowArray.push(_allPriceData[companiesToPlot[company]][i]);
+    }
+
+    data.addRow(rowArray);
+  }
+
+  var colors;
+  if (companiesToPlot.length > 1) {
+    colors = ['#dc3912', '#3366cc', '#ff9900'];
+  } else if (companiesToPlot[0] == "Microsoft") {
+    colors = ['#3366cc'];
+  } else if (companiesToPlot[0] == 'Yahoo') {
+    colors = ['#dc3912'];
+  } else {
+    colors = ['#ff9900'];
+  }
+
+  // Now, define options
+  var options = {
+    width : CHART_WIDTH,
+    height: CHART_HEIGHT,
+    vAxis: {title : 'Price in USD($)'},
+    hAxis: {title: 'Day ranging from March 30, 2011 to March 30, 2012'},
+    seriesType: 'line',
+    colors: colors
+  }
+
+  var chart = new google.visualization.LineChart(document.getElementById(elementId));
+  chart.draw(data, options);
+}
+
+function findMaxEntries() {
+  var numPrices = 0;
+  for(var company in _allPriceData){
+    if (!_allPriceData.hasOwnProperty(company)){continue;}
+    if(_allPriceData[company].length > numPrices){
+      numPrices = _allPriceData[company].length;
+    }
+  }
+
+  _allPriceData['days'] = new Array();
+  for(var i = 1; i <= numPrices; i++){
+    _allPriceData['days'].push(i);
+  }
+
+  return numPrices;
+}
+
+{% endblock extra %}
+
+{% block visuals %}
+[ 
+// render visual 1
+// note this when called is the '#interactive-n' jQuery div
+function (data) {
+  drawChart(Object.keys(_allCompanyData), this.attr('id'));
+},
+
+function (data) {
+  drawChart([data[1] || 'Microsoft'], this.attr('id'));
+},
+
+function (data) {
+  drawChart([data[1] || 'Microsoft'], this.attr('id'));
+}
+]
+{% endblock visuals %}
+
+
+
